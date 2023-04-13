afb4813f192d456247f411067df72967fc41ec27
nitu8860 <nitu8860@gmail.com>
2023-04-13 20:06:02 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("This application is supposed to send mails");
+
+        HandleMail mailer = new HandleMail();
+        mailer.sendMail();
+    }
+}
